b18131a0c0cfb1dde03b0a21fb0e35d6ecf4f61e
yaoyunting <yysay@live.com>
2016-12-15 17:12:40 +0800

    报表

--- ReportSystem/ViewModels/MainWindowViewModel.cs
+++ ReportSystem/ViewModels/MainWindowViewModel.cs
@@ -25,6 +25,7 @@ namespace ReportSystem.ViewModels
                  ViewModelSource.Create(()=>new ModuleInfo("ContractCollectionView",this,"合同列表")).SetIcon("GridContacts"),
                  ViewModelSource.Create(()=>new ModuleInfo("LoanCollocationView",this,"放款")).SetIcon("GridContacts"),
                  ViewModelSource.Create(()=>new ModuleInfo("PremiumCollectionView",this,"保费信息")).SetIcon("GridContacts"),
+                 ViewModelSource.Create(()=>new ModuleInfo("ReportCollectionView",this,"报表信息")).SetIcon("GridContacts"),
                  ViewModelSource.Create(()=>new ModuleInfo("CompanySettingView",this,"设置")).SetIcon("GridContacts")
 
             };
